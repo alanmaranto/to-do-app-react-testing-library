1991fa79c88aa732d85af738fa084945a346c8e8
alan <alanarostegui@hotmail.com>
2021-10-10 22:22:28 -0500

    feat: todo footer unit test

---
+++ src/__tests__/components/TodoFooter.test.js
@@ -0,0 +1,58 @@
+import { render, screen } from "@testing-library/react";
+import ToDoFooter from "../../components/TodoFooter/TodoFooter";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const MockToDoFooter = ({ numberOfIncompleteTasks }) => (
+  <Router>
+    <ToDoFooter numberOfIncompleteTasks={numberOfIncompleteTasks} />
+  </Router>
+);
+
+it("should render the correct amount of incomplete tasks", () => {
+  render(<MockToDoFooter numberOfIncompleteTasks={5} />);
+  const paragraphElement = screen.getByText(/5 tasks left/i);
+  expect(paragraphElement).toBeInTheDocument();
+});
+
+it("should render task when the number of complete tasks is one", () => {
+  render(<MockToDoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByText(/1 task left/i);
+  expect(paragraphElement).toBeInTheDocument();
+});
+
+it("should render task when the number of complete tasks is one with to be truthy", () => {
+  render(<MockToDoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByText(/1 task left/i);
+  expect(paragraphElement).toBeTruthy();
+});
+
+// this will fail is opacity is 0
+/* it("should render task when the number of complete tasks is one with to be visible", () => {
+  render(<MockToDoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByText(/1 task left/i);
+  expect(paragraphElement).toBeVisible();
+}); */
+
+it("should render task when the number of complete tasks is one with to p tag", () => {
+  render(<MockToDoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByText(/1 task left/i);
+  expect(paragraphElement).toContainHTML("p");
+});
+
+it("should render task when the number of complete tasks is one with test id", () => {
+  render(<MockToDoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByTestId("task");
+  expect(paragraphElement).toHaveTextContent("1 task left");
+});
+
+it("should render task when the number of complete tasks is one with test id and not to be falsy", () => {
+  render(<MockToDoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByTestId("task");
+  expect(paragraphElement).not.toBeFalsy();
+});
+
+it("should render task when the number of complete tasks is one with text content of the element", () => {
+  render(<MockToDoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByTestId("task");
+  expect(paragraphElement.textContent).toBe("1 task left");
+});
--- src/components/TodoFooter/TodoFooter.js
+++ src/components/TodoFooter/TodoFooter.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 function TodoFooter({ numberOfIncompleteTasks }) {
   return (
     <div className="todo-footer">
-      <p>
+      <p style={{ opacity: 0 }} data-testid="task">
         {numberOfIncompleteTasks}{" "}
         {numberOfIncompleteTasks === 1 ? "task" : "tasks"} left
       </p>
